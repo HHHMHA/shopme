d61420e334bc286e177585aa9e993832efc8eed1
J2mF <mp4-12cs5@hotmail.com>
2021-09-07 21:51:20 +0300

    Setup Project Structure

---
+++ shopme-web-parent/shopme-frontend/src/main/java/org/thekiddos/site/MainController.java
@@ -0,0 +1,12 @@
+package org.thekiddos.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index.html";
+    }
+}
---
+++ shopme-web-parent/shopme-frontend/src/main/java/org/thekiddos/site/ShopmeFrontendApplication.java
@@ -0,0 +1,13 @@
+package org.thekiddos.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeFrontendApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( ShopmeFrontendApplication.class, args );
+    }
+
+}
